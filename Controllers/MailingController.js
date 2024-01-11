d7e57cece2740808055e44f45bc08b1946de515e
yelmouss <yelmouss.devt@gmail.com>
2024-01-11 14:58:51 +0100

    Add .gitignore to ignore .env

--- Controllers/MailingController.js
+++ Controllers/MailingController.js
@@ -20,12 +20,17 @@ exports.sendEmail = async (req, res) => {
 
         // Message personnalisé avec HTML
         const customHtml = `
-        <div style="font-family: Arial, sans-serif;margin: 0 auto; padding: 20px; background-color: #1E2022;">
-            <h2 style="color: #C9D6DF; text-align: center;">Bonjour,</h2>
-            <p style="color: #C9D6DF;">Nous avons bien reçu votre message :</p>
-            <p style="color: #C9D6DF; margin-bottom: 20px;"><strong>${html}</strong></p>
-            <p style="color: #C9D6DF;">Merci de nous avoir contactés!</p>
+        <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #F0F5F9;">
+
+        <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #1E2022; color: #C9D6DF;">
+            <img src="https://yelmouss.vercel.app/static/media/Geek.f6f97d62b3ed6895e2b9.png" alt="Logo" style="display: block; margin: 0 auto; max-width: 100%; height: auto;">
+            <h2 style="text-align: center;">Bonjour,</h2>
+            <p>Nous avons bien reçu votre message :</p>
+            <p style="margin-bottom: 20px;"><strong>${html}</strong></p>
+            <p>Merci de nous avoir contactés!</p>
         </div>
+    
+    </body>
     `;
 
         const mailOptions = {
